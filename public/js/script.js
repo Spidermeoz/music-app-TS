7fc2da1858b7c46b488e9c2cbcd42cb71df9789f
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-08 17:19:01 +0700

    Làm nút bài hát yêu thích trang chi tiết bài hát

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
 // APlayer
 const aplayer = document.querySelector("#aplayer");
 if (aplayer) {
-    let dataSong = aplayer.getAttribute("data-song")
-    dataSong = JSON.parse(dataSong)
+  let dataSong = aplayer.getAttribute("data-song");
+  dataSong = JSON.parse(dataSong);
 
-    let dataSinger = aplayer.getAttribute("data-singer")
-    dataSinger = JSON.parse(dataSinger)
+  let dataSinger = aplayer.getAttribute("data-singer");
+  dataSinger = JSON.parse(dataSinger);
 
   const ap = new APlayer({
     container: document.getElementById("aplayer"),
@@ -17,43 +17,71 @@ if (aplayer) {
         cover: dataSong.avatar,
       },
     ],
-    autoplay: true
+    autoplay: true,
   });
 
-  const avatar = document.querySelector(".singer-detail .inner-avatar")
+  const avatar = document.querySelector(".singer-detail .inner-avatar");
 
-  ap.on('play', function () {
-    avatar.style.animationPlayState = "running"
-  })
+  ap.on("play", function () {
+    avatar.style.animationPlayState = "running";
+  });
 
-  ap.on('pause', function () {
-    avatar.style.animationPlayState = "paused"
-  })
+  ap.on("pause", function () {
+    avatar.style.animationPlayState = "paused";
+  });
 } // End APlayer
 
 // Button like
-const buttonLike = document.querySelector("[button-like]")
-if(buttonLike){
-    buttonLike.addEventListener("click", () => {
-        const idSong = buttonLike.getAttribute("button-like")
-        const isActive = buttonLike.classList.contains("active")
+const buttonLike = document.querySelector("[button-like]");
+if (buttonLike) {
+  buttonLike.addEventListener("click", () => {
+    const idSong = buttonLike.getAttribute("button-like");
+    const isActive = buttonLike.classList.contains("active");
 
-        const typeLike = isActive ? "dislike" : "like"
+    const typeLike = isActive ? "dislike" : "like";
 
-        const link = `/songs/like/${typeLike}/${idSong}`
+    const link = `/songs/like/${typeLike}/${idSong}`;
 
-        const option = {
-            method: "PATCH"
+    const option = {
+      method: "PATCH",
+    };
+
+    fetch(link, option)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.code == 200) {
+          const span = buttonLike.querySelector("span");
+          span.innerHTML = `${data.like} thích`;
+
+          buttonLike.classList.toggle("active");
         }
-
-        fetch(link, option)
-            .then(res => res.json())
-            .then(data => {
-                const span = buttonLike.querySelector("span")
-                span.innerHTML = `${data.like} thích`
-
-                buttonLike.classList.toggle("active")
-            })
-    })
+      });
+  });
 }
 // End Button like
+
+// Button favorite
+const buttonFavorite = document.querySelector("[button-favorite]");
+if (buttonFavorite) {
+  buttonFavorite.addEventListener("click", () => {
+    const idSong = buttonFavorite.getAttribute("button-favorite");
+    const isActive = buttonFavorite.classList.contains("active");
+
+    const typeFavorite = isActive ? "unfavorite" : "favorite";
+
+    const link = `/songs/favorite/${typeFavorite}/${idSong}`;
+
+    const option = {
+      method: "PATCH",
+    };
+
+    fetch(link, option)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.code == 200) {
+          buttonFavorite.classList.toggle("active");
+        }
+      });
+  });
+}
+// End Button favorite
