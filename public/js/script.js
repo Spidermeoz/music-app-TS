7dcd1459b4f9ff505980005c679addf1e94d5280
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-08 23:27:17 +0700

    Tìm kiếm hiển thị suggest (gợi ý)

--- public/js/script.js
+++ public/js/script.js
@@ -57,8 +57,7 @@ if (buttonLike) {
         }
       });
   });
-}
-// End Button like
+} // End Button like
 
 // Button favorite
 const listButtonFavorite = document.querySelectorAll("[button-favorite]");
@@ -87,5 +86,54 @@ if (listButtonFavorite) {
       });
     });
   }
+} // End Button favorite
+
+// Search Suggest
+const boxSearch = document.querySelector(".box-search");
+if (boxSearch) {
+  const input = boxSearch.querySelector("input[name='keyword']");
+  const boxSuggest = boxSearch.querySelector(".inner-suggest");
+  input.addEventListener("keyup", () => {
+    const keyword = input.value;
+
+    const link = `/search/suggest?keyword=${keyword}`;
+
+    fetch(link)
+      .then((res) => res.json())
+      .then((data) => {
+        const songs = data.songs;
+        if (songs.length > 0) {
+          boxSuggest.classList.add("show");
+
+          const htmls = songs.map((song) => {
+            return `
+              <a class="inner-item" href="/songs/detail/${song.slug}">
+                <div class="inner-image">
+                  <img src=${song.avatar} />
+                </div>
+                <div class="inner-info">
+                  <div class="inner-title">${song.title}</div>
+                  <div class="inner-singer">
+                    <i class="fa-solid fa-microphone-lines"></i> ${song.infoSinger.fullName}
+                  </div>
+                </div>
+              </a>
+            `;
+          });
+
+          const boxList = boxSuggest.querySelector(".inner-list");
+          boxList.innerHTML = htmls.join("");
+        } else {
+          boxSuggest.classList.remove("show");
+        }
+      });
+  });
+  
+  // Ẩn box-search khi click ra ngoài
+  document.addEventListener("click", (e) => {
+    if (!boxSearch.contains(e.target)) {
+      boxSuggest.classList.remove("show");
+    }
+  });
 }
-// End Button favorite
+// End Search Suggest
