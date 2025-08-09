3d53eba75074542a55c758ce6bafd08b555e4532
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-09 17:44:07 +0700

    buld folder dist

--- index.ts
+++ index.ts
@@ -18,9 +18,9 @@ database.connect();
 
 app.use(methodOverride("_method"));
 
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
-app.set("views", `./views`);
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 app.use(express.json());
