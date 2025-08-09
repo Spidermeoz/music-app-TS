3a3502cd21bb1e6484676e93b2501e34e10bb4fc
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-09 10:14:29 +0700

    Layouts chung bên Admin

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import path from "path"
 
 import * as database from "./config/database";
 
 import clientRoutes from "./routes/client/index.route";
+import adminRoutes from "./routes/admin/index.route";
+import { systemConfig } from "./config/config";
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
@@ -17,6 +20,19 @@ app.use(express.static("public"))
 app.set("views", `./views`);
 app.set("view engine", "pug");
 
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+// Admin routes
+adminRoutes(app);
+
+// Client routes
 clientRoutes(app);
 
 app.listen(port, () => {
